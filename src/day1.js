b00855684d825a5c22d9cd6cf9137c8a9bafa530
Carl St-Laurent <carl@carlstlaurent.com>
2015-12-01 23:06:49 -0500

    Added standard JS and corrected code to repair linting errors

--- src/day1.js
+++ src/day1.js
@@ -5,18 +5,18 @@ let data = `()(((()))(()()()((((()(((())(()(()((((((()(()(((())))((()(((()))((()
 let currentFloor = 0
 let enteredBasement = false
 for (let index = 0; index < data.length; index++) {
-	let element = data[index]
-	
-	if(element === '('){
-		currentFloor++
-	} else if(element === ')'){
-		currentFloor--
-	}
-	
-	if(!enteredBasement && currentFloor < 0){
-		console.log('Santa entered basement at position: ' + (index + 1)) //Part 2
-		enteredBasement = true
-	}
+  let element = data[index]
+
+  if (element === '(') {
+    currentFloor++
+  } else if (element === ')') {
+    currentFloor--
+  }
+
+  if (!enteredBasement && currentFloor < 0) {
+    console.log('Santa entered basement at position: ' + (index + 1)) // Part 2
+    enteredBasement = true
+  }
 }
 
-console.log('Santa finishes at floor: ' + currentFloor) //Part 1
+console.log('Santa finishes at floor: ' + currentFloor) // Part 1
