0cf9758d467f39d9aae027a8c9d99f688770af0c
unknown <cstlaurent@CSTLAURENT.GENETEC.COM>
2015-12-03 21:49:46 -0500

    finalized day 3 part 1

--- src/day2/day2.specs.js
+++ src/day2/day2.specs.js
@@ -1,14 +1,1 @@
 'use strict'
-
-let day2 = require('./day2')
-let should = require('chai').should()
-
-describe('homepage', function(){
-  it('should respond to GET',function(done){
-    superagent
-      .get('http://localhost:'+port)
-      .end(function(res){
-        expect(res.status).to.equal(200);
-        done();
-    })
-  })
--- src/day3/day3.js
+++ src/day3/day3.js
@@ -1,2 +1,42 @@
 'use strict'
 
+let fs = require('fs')
+
+let posX = 0
+let posY = 0
+
+let houses = {}
+houses[posX + '|' + posY] = true
+
+let dataStream = fs.createReadStream('src/day3/day3data.txt', {
+  encoding: 'utf8'
+})
+
+dataStream.on('readable', () => {
+  let caracter
+  while ((caracter = dataStream.read(1)) !== null) {
+    switch (caracter) {
+      case '<':
+        posX--
+        break
+      case '^':
+        posY++
+        break
+      case '>':
+        posX++
+        break
+      case 'v':
+        posY--
+        break
+      default:
+        // Invalid char, do nothing
+        break
+    }
+    houses[posX + '|' + posY] = true
+  }
+})
+
+dataStream.on('end', () => {
+  var keyCount = Object.keys(houses).length
+  console.log('Santa delivered presents to ' + keyCount + ' different houses')
+})
