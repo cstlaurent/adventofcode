de941f4ec56c3b7e96dbbb0c29d89782ea83dcb2
Carl St-Laurent <carl@carlstlaurent.com>
2015-12-05 00:38:14 -0500

    cleaned up copied code from last questions

--- src/day5/day5.js
+++ src/day5/day5.js
@@ -4,19 +4,23 @@
 let lineReader = require('line-reader')
 
 module.exports = {
-  calculateNeededPaper: calculateNeededPaper
+  part1: part1,
+  part2: part2
 }
 
-let niceWords = 0
+let niceWords1 = 0
+let niceWords2 = 0
 
 lineReader.eachLine('src/day5/day5data.txt', function (line, last) {
+  part1(line)
   part2(line)
   if (last) {
-    console.log('Good words total: ' + niceWords)
+    console.log('Good words total for part 1: ' + niceWords1)
+    console.log('Good words total for part 2: ' + niceWords2)
   }
 })
 
-function calculateNeededPaper (line) {
+function part1 (line) {
   let vowel = line.match(/[aeiou]/ig)
   if (vowel !== null && vowel.length >= 3) {
     let doubleletter = line.match(/([a-z])\1/i)
@@ -25,7 +29,7 @@ function calculateNeededPaper (line) {
         line.indexOf('cd') === -1 &&
         line.indexOf('pq') === -1 &&
         line.indexOf('xy') === -1) {
-        niceWords++
+        niceWords1++
       }
     }
   }
@@ -36,7 +40,7 @@ function part2 (line) {
   if (dual !== null) {
     let repeater = line.match(/([A-Za-z]{1})[a-z]{1}\1/ig)
     if (repeater !== null) {
-      niceWords++
+      niceWords2++
     }
   }
 }
